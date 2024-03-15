cb29e780f6aa382304e46663b7a394c5338d906e
mustafatavasli <musstafatavasli@gmail.com>
2024-03-15 17:22:15 +0300

    Initial Commit

---
+++ FilmKitabi/FilmKitabiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmKitabiApp.swift
+//  FilmKitabi
+//
+//  Created by Mustafa TAVASLI on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FilmKitabiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
