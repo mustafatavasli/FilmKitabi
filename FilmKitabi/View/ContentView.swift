f2fa1221c1105f7cf76b740f53cd8da02a4c90e6
mustafatavasli <musstafatavasli@gmail.com>
2024-03-18 11:28:47 +0300

    FilmViewModel Created

--- FilmKitabi/View/ContentView.swift
+++ FilmKitabi/View/ContentView.swift
@@ -10,14 +10,6 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Button(action: {
-            DownloaderClient().filmleriIndir(search: "pulp") { (sonuc) in
-                switch sonuc {
-                case .success(let filmDizisi) :
-                    print(filmDizisi)
-                case .failure(let hata) :
-                    print(hata)
-                }
-            }
         }, label: {
             Text("Start Testing")
         })
